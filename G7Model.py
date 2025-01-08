49edd49d3a70c6795cd026c72281f6e670e8985f
Anant2003jain <jainanant892@gmail.com>
2025-01-08 12:27:10 +0530

    first release

---
+++ G7Model.py
@@ -0,0 +1,96 @@
+import os
+import cv2
+import csv
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, Flatten, Dense, MaxPooling2D, Dropout
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+
+# Load label mapping
+label_dict = {}
+with open(r'StudentData//class.csv', mode='r') as csvfile:
+    csvreader = csv.DictReader(csvfile) 
+    for row in csvreader:
+        label_dict[int(row['classid'])] = row['classname']
+
+# Function to load and preprocess images for training
+def load_images_from_folder(folder):
+    images = []
+    for filename in os.listdir(folder):
+        img = cv2.imread(os.path.join(folder, filename))
+        if img is not None:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)  # Convert image to grayscale
+            img = cv2.resize(img, (64, 64))  # Resize images to a fixed size
+            img = img / 255.0  # Normalize pixel values to be between 0 and 1
+            images.append(img)
+    return np.array(images)
+
+def G7model_Train():
+    # Load training data
+    train_data = []
+    labels = []
+    for folder_name in os.listdir(r'StudentData//images'):
+        folder_path = os.path.join(r'StudentData//images', folder_name)
+        images = load_images_from_folder(folder_path)
+        train_data.extend(images)
+        labels.extend([folder_name] * len(images))
+    
+    train_data = np.array(train_data)
+    labels = np.array(labels)
+    
+    # Convert labels to numerical values
+    label_encoder = LabelEncoder()
+    labels = label_encoder.fit_transform(labels)
+    
+    # Data augmentation
+    datagen = ImageDataGenerator(rotation_range=20, width_shift_range=0.1, height_shift_range=0.1,
+                                 shear_range=0.2, zoom_range=0.2, horizontal_flip=True)
+    
+    # Split data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(train_data, labels, test_size=0.2, random_state=42)
+    
+    # Reshape data to fit the model
+    X_train = np.reshape(X_train, (X_train.shape[0], 64, 64, 1))
+    X_test = np.reshape(X_test, (X_test.shape[0], 64, 64, 1))
+    
+    # Define the CNN model
+    model = Sequential([
+        Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 1), kernel_regularizer='l2'),
+        MaxPooling2D(2, 2),
+        
+        Conv2D(64, (3, 3), activation='relu', kernel_regularizer='l2'),
+        MaxPooling2D(2, 2),
+        
+        Flatten(),
+        Dropout(0.5),
+        
+        Dense(128, activation='relu', kernel_regularizer='l2'),
+        Dropout(0.5),
+        
+        Dense(len(np.unique(labels)), activation='softmax')
+    ])
+    
+    # Compile the model
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    
+    # Train the model with data augmentation
+    model.fit(datagen.flow(X_train, y_train, batch_size=32), epochs=20, validation_data=(X_test, y_test))
+    
+    # Evaluate the model
+    model.evaluate(X_test, y_test)
+    
+    # Evaluate the model
+    print("[INFO] Evaluating the model...")
+    predictions = model.predict(X_test, batch_size=32)
+    print(classification_report(y_test, np.argmax(predictions, axis=1), target_names=label_encoder.classes_))
+    
+    # Save the model to a file
+    model.save(r'Model//new_face_recognition_model.h5')
+    
+    print(model.summary())
+
+
+#G7model_Train()
---
+++ RemoveData.py
@@ -0,0 +1,50 @@
+import os
+import send2trash
+import pandas as pd
+
+def remove_user():
+    try:
+        firstname = input("Enter First Name: ").strip().title()
+        lastname = input("Enter Last Name: ").strip().title()
+        enroll = input("Enter Enrollment No: ").strip().upper()
+        
+        nameID = f"{firstname}{lastname}-{enroll}"
+
+        csv_file_path = r"D:\Dl_Model\StudentData\class.csv"
+        image_folder_path = os.path.join(r"D:\Dl_Model\StudentData\images", nameID)
+        qr_code_path = os.path.join(r"D:\Dl_Model\StudentData\QR", f"{nameID}.png")
+
+        # Read CSV file
+        df = pd.read_csv(csv_file_path)
+
+        # Find user index
+        index_to_remove = df.index[df['classname'] == nameID].tolist()
+
+        if index_to_remove:
+            # Remove user details from CSV file
+            df.drop(index_to_remove, inplace=True)
+            df.to_csv(csv_file_path, index=False)
+            print("\nUser details removed from CSV.")
+
+            # Delete folder and QR code
+            if os.path.exists(image_folder_path):
+                try:
+                    send2trash.send2trash(image_folder_path)  # Recursively remove directory and its contents
+                    print("Image folder deleted.")
+                except OSError as e:
+                    print(f"Error: {e}")
+            else:
+                print("Image folder not found.")
+
+            if os.path.exists(qr_code_path):
+                os.remove(qr_code_path)
+                print("QR code deleted.")
+            else:
+                print("QR code not found.")
+        else:
+            print("User not found in the CSV file.")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+#remove_user()
